06d91de3a37c533e7b7abe179f08084d966498ba
samgerene <sam@gerene.nl>
2026-03-15 12:05:46 +0100

    [Add] GuidExtensions

---
+++ ArgusTransfer.Tests/Extensions/GuidExtensionsTestFixture.cs
@@ -0,0 +1,150 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="GuidExtensionsTestFixture.cs" >
+//   Copyright (C) 2026 Sam Gerené
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//   Unless required by applicable law or agreed to in writing, softwareUseCases
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// 
+// </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace ArgusTransfer.Tests.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using ArgusTransfer.Extensions;
+    
+    using NUnit.Framework;
+
+    /// <summary>
+    /// Suite of tests for the <see cref="GuidExtensions"/> class
+    /// </summary>
+    [TestFixture]
+    public class GuidExtensionsTestFixture
+    {
+        [Test]
+        public void Verify_that_ToShortGuid_from_string_returns_valid_short_guid()
+        {
+            var guidString = "cfb2e590-eed6-4223-b2ba-271ed0cb06da";
+
+            var shortGuid = guidString.ToShortGuid();
+
+            Assert.That(shortGuid, Is.Not.Null.And.Not.Empty);
+            Assert.That(shortGuid.Length, Is.EqualTo(22));
+        }
+
+        [Test]
+        public void Verify_that_ToShortGuid_from_Guid_returns_valid_short_guid()
+        {
+            var guid = Guid.Parse("cfb2e590-eed6-4223-b2ba-271ed0cb06da");
+
+            var shortGuid = guid.ToShortGuid();
+
+            Assert.That(shortGuid, Is.Not.Null.And.Not.Empty);
+            Assert.That(shortGuid.Length, Is.EqualTo(22));
+        }
+
+        [Test]
+        public void Verify_that_FromShortGuid_returns_original_Guid()
+        {
+            var originalGuid = Guid.Parse("cfb2e590-eed6-4223-b2ba-271ed0cb06da");
+
+            var shortGuid = originalGuid.ToShortGuid();
+            var roundTripped = shortGuid.FromShortGuid();
+
+            Assert.That(roundTripped, Is.EqualTo(originalGuid));
+        }
+
+        [Test]
+        public void Verify_that_ToShortGuids_from_strings_returns_valid_short_guids()
+        {
+            var guidStrings = new List<string>
+            {
+                "cfb2e590-eed6-4223-b2ba-271ed0cb06da",
+                "fe08550c-d926-4758-808f-a9a991e0ff37"
+            };
+
+            var shortGuids = guidStrings.ToShortGuids().ToList();
+
+            Assert.That(shortGuids.Count, Is.EqualTo(2));
+            Assert.That(shortGuids.All(sg => sg.Length == 22), Is.True);
+        }
+
+        [Test]
+        public void Verify_that_ToShortGuids_from_Guids_returns_valid_short_guids()
+        {
+            var guids = new List<Guid>
+            {
+                Guid.Parse("cfb2e590-eed6-4223-b2ba-271ed0cb06da"),
+                Guid.Parse("fe08550c-d926-4758-808f-a9a991e0ff37")
+            };
+
+            var shortGuids = guids.ToShortGuids().ToList();
+
+            Assert.That(shortGuids.Count, Is.EqualTo(2));
+            Assert.That(shortGuids.All(sg => sg.Length == 22), Is.True);
+        }
+
+        [Test]
+        public void Verify_that_ToShortGuidArray_returns_bracket_delimited_string()
+        {
+            var guids = new List<Guid>
+            {
+                Guid.Parse("cfb2e590-eed6-4223-b2ba-271ed0cb06da"),
+                Guid.Parse("fe08550c-d926-4758-808f-a9a991e0ff37")
+            };
+
+            var array = guids.ToShortGuidArray();
+
+            Assert.That(array, Does.StartWith("["));
+            Assert.That(array, Does.EndWith("]"));
+            Assert.That(array, Does.Contain(";"));
+        }
+
+        [Test]
+        public void Verify_that_FromShortGuidArray_returns_original_Guids()
+        {
+            var guids = new List<Guid>
+            {
+                Guid.Parse("cfb2e590-eed6-4223-b2ba-271ed0cb06da"),
+                Guid.Parse("fe08550c-d926-4758-808f-a9a991e0ff37")
+            };
+
+            var array = guids.ToShortGuidArray();
+            var roundTripped = array.FromShortGuidArray().ToList();
+
+            Assert.That(roundTripped.Count, Is.EqualTo(2));
+            Assert.That(roundTripped[0], Is.EqualTo(guids[0]));
+            Assert.That(roundTripped[1], Is.EqualTo(guids[1]));
+        }
+
+        [Test]
+        public void Verify_that_FromShortGuidArray_throws_when_missing_opening_bracket()
+        {
+            var invalidArray = "abc]";
+
+            Assert.That(() => invalidArray.FromShortGuidArray().ToList(),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Verify_that_FromShortGuidArray_throws_when_missing_closing_bracket()
+        {
+            var invalidArray = "[abc";
+
+            Assert.That(() => invalidArray.FromShortGuidArray().ToList(),
+                Throws.TypeOf<ArgumentException>());
+        }
+    }
+}
---
+++ ArgusTransfer/Extensions/GuidExtensions.cs
@@ -0,0 +1,194 @@
+﻿// -------------------------------------------------------------------------------------------------
+// <copyright file="GuidExtensions.cs">
+// 
+//   Copyright (C) 2026 Sam Gerené
+// 
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+//   Unless required by applicable law or agreed to in writing, softwareUseCases
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// 
+// </copyright>
+//  ------------------------------------------------------------------------------------------------
+
+namespace ArgusTransfer.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// static extension methods for <see cref="Guid"/>
+    /// </summary>
+    public static class GuidExtensions
+    {
+        /// <summary>
+        /// Converts a <see cref="Guid"/> string representation to a ShortGuid
+        /// </summary>
+        /// <param name="value">
+        /// a string representation of a <see cref="Guid"/>
+        /// </param>
+        /// <returns>
+        /// a ShortGuid representation of the provided <see cref="Guid"/>
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref="Uri"/> friendly)
+        /// </remarks>
+        public static string ToShortGuid(this string value)
+        {
+            var guid = new Guid(value);
+            return ToShortGuid(guid);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="IEnumerable{String}"/> <see cref="Guid"/> string representation to a ShortGuid
+        /// </summary>
+        /// <param name="values">
+        /// an <see cref="IEnumerable{String}"/> representation of a <see cref="Guid"/>s
+        /// </param>
+        /// <returns>
+        /// a <see cref="IEnumerable{String}"/> ShortGuid representation of the provided <see cref="Guid"/>s
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref="Uri"/> friendly)
+        /// </remarks>
+        public static IEnumerable<string> ToShortGuids(this IEnumerable<string> values)
+        {
+            foreach (var value in values)
+            {
+                var guid = new Guid(value);
+                yield return ToShortGuid(guid);
+            }
+        }
+
+        /// <summary>
+        /// converts a <see cref="Guid"/> to its base64 encoded short form
+        /// </summary>
+        /// <param name="guid">
+        /// an instance of <see cref="Guid"/>
+        /// </param>
+        /// <returns>
+        /// a shortGuid representation of the provided <see cref="Guid"/>
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref="Uri"/> friendly)
+        /// </remarks>
+        public static string ToShortGuid(this Guid guid)
+        {
+            var enc = Convert.ToBase64String(guid.ToByteArray());
+            return enc.Replace("/", "_").Replace("+", "-").Substring(0, 22);
+        }
+
+        /// <summary>
+        /// Converts a <see cref="IEnumerable{Guid}"/> to an <see cref="IEnumerable{String}"/> ShortGuid
+        /// </summary>
+        /// <param name="guids">
+        /// an <see cref="IEnumerable{Guid}"/>
+        /// </param>
+        /// <returns>
+        /// a <see cref="IEnumerable{String}"/> ShortGuid representation of the provided <see cref="Guid"/>s
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref="Uri"/> friendly)
+        /// </remarks>
+        public static IEnumerable<string> ToShortGuids(this IEnumerable<Guid> guids)
+        {
+            foreach (var guid in guids)
+            {
+                yield return ToShortGuid(guid);
+            }
+        }
+
+        /// <summary>
+        /// Creates a <see cref="Guid"/> based the ShortGuid representation
+        /// </summary>
+        /// <param name="shortGuid">
+        /// a shortGuid string
+        /// </param>
+        /// <returns>
+        /// an instance of <see cref=".Guid"/>
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref=".Uri"/> friendly)
+        /// </remarks>
+        public static Guid FromShortGuid(this string shortGuid)
+        {
+            var buffer = Convert.FromBase64String(shortGuid.Replace("_", "/").Replace("-", "+") + "==");
+            return new Guid(buffer);
+        }
+
+        /// <summary>
+        /// Creates a <see cref="IEnumerable{Guid}"/> based the ShortGuid Array representation -> 
+        /// </summary>
+        /// <param name="shortGuids">
+        /// an <see cref="IEnumerable{String}"/> shortGuid 
+        /// </param>
+        /// <returns>
+        /// an <see cref="IEnumerable{Guid}"/>
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref=".Uri"/> friendly)
+        /// A ShortGuid Array is a string that starts with "[", ends with "]" and contains a number of ShortGuid separated by a ";"
+        /// </remarks>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the <paramref name="shortGuids"/> does not start with '[' or ends with ']'
+        /// </exception>
+        public static IEnumerable<Guid> FromShortGuidArray(this string shortGuids)
+        {
+            if (!shortGuids.StartsWith("["))
+            {
+                throw new ArgumentException("Invalid ShortGuid Array, must start with [", nameof(shortGuids));
+            }
+
+            if (!shortGuids.EndsWith("]"))
+            {
+                throw new ArgumentException("Invalid ShortGuid Array, must end with ]", nameof(shortGuids));
+            }
+
+            var listOfShortGuids = shortGuids.TrimStart('[').TrimEnd(']').Split(';');
+
+            foreach (var shortGuid in listOfShortGuids)
+            {
+                yield return shortGuid.FromShortGuid();
+            }
+        }
+
+        /// <summary>
+        /// Converts a <see cref="IEnumerable{Guid}"/> to an Array of ShortGuid
+        /// </summary>
+        /// <param name="guids">
+        /// an <see cref="IEnumerable{Guid}"/>
+        /// </param>
+        /// <returns>
+        /// a <see cref="IEnumerable{String}"/> ShortGuid representation of the provided <see cref="Guid"/>s
+        /// </returns>
+        /// <remarks>
+        /// A ShortGuid is a base64 encoded guid-string representation where any "/" has been replaced with a "_"
+        /// and any "+" has been replaced with a "-" (to make the string representation <see cref="Uri"/> friendly)
+        /// A ShortGuid Array is a string that starts with "[", ends with "]" and contains a number of ShortGuid separated by a ";"
+        /// </remarks>
+        public static string ToShortGuidArray(this IEnumerable<Guid> guids)
+        {
+            var shortGuids = new List<string>();
+
+            foreach (var guid in guids)
+            {
+                shortGuids.Add(ToShortGuid(guid));
+            }
+
+            return "[" + string.Join(";", shortGuids) + "]";
+        }
+    }
+}
